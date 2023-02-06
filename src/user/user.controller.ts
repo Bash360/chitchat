3c831cdcd8a093fc639e987939063eeb1f2a1a55
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-06 15:09:14 +0100

    added user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from './models/user.model';
+import { CreateUserDTO } from './dto/create-user.dto';
 
+@ApiTags('user')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  async findAll(): Promise<User[]> {
+    return [];
+  }
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<User[]> {
+    return [];
+  }
+  @Post()
+  async createUser(@Body() createUser: CreateUserDTO): Promise<User[]> {
+    return [];
+  }
+
+  @Patch(':id')
+  async updateUser(@Param('id') id: string): Promise<User> {
+    return new User();
+  }
+}
