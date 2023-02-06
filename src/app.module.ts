18e4181633b50a7f2ce677c5d06b3e6652b7505d
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-07 00:10:25 +0100

    added chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
+import { ChatModule } from './chat/chat.module';
 
 
 @Module({
@@ -11,6 +12,7 @@ import { GroupModule } from './group/group.module';
     MongooseModule.forRoot(process.env.MONGO_URL),
     UserModule,
     GroupModule,
+    ChatModule,
   ],
   controllers: [],
   providers: [],
