81117538fa753343968d495af1e20c83b88ace2c
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-06 12:01:07 +0100

    created user feature module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { UserControllerController } from './user.controller/user.controller.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UserModule,
   ],
-  controllers: [],
+  controllers: [UserControllerController],
   providers: [],
 })
 export class AppModule {}
