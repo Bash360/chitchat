110ba4bb644fe25dced8a1c0a72ae4621e492aa7
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-06 14:54:27 +0100

    added user schema to user module

---
+++ src/common/constants.ts
@@ -0,0 +1,2 @@
+export const defaultAvatarURI =
+  'https://res.cloudinary.com/bash360/image/upload/v1675690776/default-avatar_nsp1pi.png';
--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,13 +1,18 @@
-import { Prop, Schema } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { defaultAvatarURI } from '../../common/constants';
 @Schema({ strict: true })
-export class user extends Document {
+export class User extends Document {
   @Prop({ required: true })
   nickname: string;
-  @Prop({ default: '' })
+  @Prop({
+    default: defaultAvatarURI,
+  })
   avatar: string;
   @Prop({ required: true })
   email: string;
   @Prop({ required: true })
   password: string;
 }
+
+export const UserSchema = SchemaFactory.createForClass(User);
