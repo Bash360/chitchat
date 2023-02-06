3993fcd26b2bb1b13c01a25b19b928c7f25b8685
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-06 15:47:19 +0100

    added sign up, get users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,29 +2,39 @@ import {
   Body,
   Controller,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
-  ParseUUIDPipe,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { User } from './models/user.model';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { PaginationDTO } from 'src/common/pagination-dto';
+import { UserService } from './user.service';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  async findAll(): Promise<User[]> {
-    return [];
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Query() paginationDTO: PaginationDTO): Promise<User[]> {
+    return this.userService.findAll(paginationDTO);
   }
+
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<User[]> {
-    return [];
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
   }
-  @Post()
-  async createUser(@Body() createUser: CreateUserDTO): Promise<User[]> {
-    return [];
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() createUser: CreateUserDTO): Promise<User> {
+    return this.userService.createUser(createUser);
   }
 
   @Patch(':id')
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './models/user.model';
+import { user } from 'src/common/constants';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [MongooseModule.forFeature([{ name: user, schema: UserSchema }])],
   controllers: [UserController],
   providers: [UserService],
 })
