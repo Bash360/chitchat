3ee024289be4eab4f578ec0b1bbf62bb1733dc33
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-09 09:35:29 +0100

    cloudinary upload working

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,17 +1,22 @@
 import {
+  BadRequestException,
   Body,
   Controller,
-  FileTypeValidator,
-  MaxFileSizeValidator,
-  ParseFilePipe,
+  HttpStatus,
+  ParseFilePipeBuilder,
   Post,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { Express } from 'express';
 import { FileService } from './file.service';
+import { throwReadableMessages } from 'src/common/helpers';
 
 @Controller('file')
 @ApiTags('fileupload')
@@ -22,20 +27,39 @@ export class FileController {
   @UseInterceptors(FileInterceptor('avatar'))
   async uploadAvatar(
     @UploadedFile(
-      new ParseFilePipe({
-        validators: [
-          new MaxFileSizeValidator({ maxSize: 5000_000 }),
-          new FileTypeValidator({
-            fileType:
-              /(^image)(\/)[jpeg,jpg,png,x\-png,bmp,gif,avif,webp,svg+xml]*/i,
-          }),
-        ],
-      }),
+      new ParseFilePipeBuilder()
+        .addMaxSizeValidator({ maxSize: 1_000_000 })
+        .addFileTypeValidator({
+          fileType:
+            /(^image)(\/)[jpeg,jpg,png,x\-png,bmp,gif,avif,webp,svg+xml]*/i,
+        })
+        .build({
+          exceptionFactory: throwReadableMessages,
+        }),
     )
     file: Express.Multer.File,
   ) {
     const result = await this.fileService.uploadImage(file);
 
     console.log(result);
+  }
+
+  @Post('chatfiles')
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'image', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+      { name: 'document', maxCount: 1 },
+    ]),
+  )
+  async uploadChatFiles(
+    @UploadedFiles()
+    files: {
+      image?: Express.Multer.File[];
+      video?: Express.Multer.File[];
+      document?: Express.Multer.File[];
+    },
+  ) {
+    console.log(files);
   }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -8,19 +8,38 @@ const parser = new DatauriParser();
 
 @Injectable()
 export class FileService {
+  private readonly cloudName = this.config.get<string>('CLOUD_NAME');
+  private readonly apiKEY = this.config.get<string>('API_KEY');
+  private readonly apiSecret = this.config.get<string>('API_SECRET');
   constructor(private readonly config: ConfigService) {}
   async uploadImage(file: Express.Multer.File): Promise<{}> {
-    const cloudName = this.config.get<string>('CLOUD_NAME');
-    const apiKEY = this.config.get<string>('API_KEY');
-    const apiSecret = this.config.get<string>('API_SECRET');
     cloudinary.v2.config({
-      cloud_name: cloudName,
-      api_key: apiKEY,
-      api_secret: apiSecret,
+      cloud_name: this.cloudName,
+      api_key: this.apiKEY,
+      api_secret: this.apiSecret,
     });
 
-    const result = await cloudinary.v2.uploader.upload(this.extractContent(file));
+    const result = await cloudinary.v2.uploader.upload(
+      this.extractContent(file),
+      { folder: 'chichat/images' },
+    );
     return { imageURL: result.secure_url };
+  }
+
+  async uploadDocument(file: Express.Multer.File): Promise<{}> {
+    const result = await cloudinary.v2.uploader.upload(
+      this.extractContent(file),
+      { folder: 'chichat/documents' },
+    );
+    return { documentURL: result.secure_url };
+  }
+
+  async uploadVideo(file: Express.Multer.File): Promise<{}> {
+    const result = await cloudinary.v2.uploader.upload(
+      this.extractContent(file),
+      { folder: 'chichat/video' },
+    );
+    return { documentURL: result.secure_url };
   }
 
   private extractContent(file: Express.Multer.File): string {
