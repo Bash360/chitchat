c7226406e7bc153a79d7eb10edb9ab19e2b20327
mark bashir-ugwi <beejayphil@gmail.com>
2023-02-10 19:19:02 +0100

    dont need the file controller

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileService } from './file.service';
-import { FileController } from './file.controller';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule],
   providers: [FileService],
-  controllers: [FileController],
   exports: [FileService],
 })
 export class FileModule {}
